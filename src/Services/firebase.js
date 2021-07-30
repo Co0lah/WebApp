c2f017e0ee898d322eedd7be61f737ef2ba3e50b
Co0lah <celes.thym@laposte.net>
2021-07-30 23:13:47 +0200

    depre updt, deploy

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
 
 var firebaseConfig = {
   apiKey: "AIzaSyB4QdbAlZBxzHIMBZOIGXt9vkNMqBvdOFk",
